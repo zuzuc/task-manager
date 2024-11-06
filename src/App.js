082c6bcc793400410f7d5c632a2879eeec7ab485
zuzuc <zuzu@posteo.de>
2024-11-06 15:10:55 +0100

    Passing handleAddTask function as props to TaskInput component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import './App.css';
 import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
 
-function taskManager() {
+function TaskManager() {
+  const [tasks, setTasks] = useState([]);
+  const handleAddTask = (newTask) => {
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+};
   return (
     <div className="App">
       <div className="title" style={{ textAlign: 'center' }}>
         <h1>Task Manager</h1>
       </div>
-      <TaskInput />
+      <TaskInput onAddTask={handleAddTask}/>
+      <TaskList tasks={tasks} />
     </div>
   );
 }
 
-export default taskManager;
+export default TaskManager;
