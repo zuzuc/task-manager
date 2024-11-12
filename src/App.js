1c99bef9281a752eab4b7e0124304cd2d63614b9
zuzuc <zuzu@posteo.de>
2024-11-12 17:09:13 +0100

    Refactor and prettify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import { useState } from "react";
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./pages/landing-page/LandingPage";
 import TaskInput from "./components/TaskInput";
 import TaskList from "./components/TaskList";
 
-function TaskManager() {
+function App() {
   const [tasks, setTasks] = useState([]);
   const handleAddTask = (newTask) => {
     setTasks((prevTasks) => [...prevTasks, newTask]); // Adds the new task to the tasks array
-};
+  };
 
   return (
     <div>
-      <div className="title" style={{ textAlign: 'center' }}>
+      <div className="title" style={{ textAlign: "center" }}>
         <h1>Task Manager</h1>
       </div>
-      <TaskInput onAddTask={handleAddTask}/>
-      <TaskList tasks={tasks} />
+      <main>
+        <Routes>
+          <Route exact path="/" element={LandingPage}>
+            <LandingPage />
+          </Route>
+          <Route exact path="/TaskInput" element={TaskInput}>
+            <TaskInput onAddTask={handleAddTask} />
+          </Route>
+          <Route exact path="/TaskInput" element={TaskInput}>
+            <TaskList tasks={tasks} />
+          </Route>
+        </Routes>
+      </main>
     </div>
   );
 }
 
-export default TaskManager;
+export default App;
