60bfa57d322f0dcb24498d03eec9ff9e5b1fda25
zuzuc <zuzu@posteo.de>
2024-11-19 12:38:19 +0100

    Update App.js to have route to TaskManagerPage - move task state logic and variables from App.js to TaskManagerPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/landing-page/LandingPage";
-import TaskInput from "./components/task-input/TaskInput";
-import TaskList from "./components/TaskList";
+import TaskManagerPage from "./pages/task-manager/TaskManagerPage";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const handleAddTask = (newTask) => {
-    setTasks((prevTasks) => [...prevTasks, newTask]); // Adds the new task to the tasks array
-  };
 
   return (
     <div>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route
-          path="/task-input"
-          element={<TaskInput onAddTask={handleAddTask} />}
-        />
-        <Route path="/task-list" element={<TaskList tasks={tasks} />} />
+        <Route path="/task-manager" element={<TaskManagerPage />} />
       </Routes>
     </div>
   );
