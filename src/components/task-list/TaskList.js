bf1541805f0fd0482e4d7c95bd942696d5623fea
zuzuc <zuzu@posteo.de>
2024-11-27 16:41:46 +0100

    Add buttons to each task item

--- src/components/task-list/TaskList.js
+++ src/components/task-list/TaskList.js
@@ -5,9 +5,26 @@ function TaskList({ tasks }) {
     <ul className="task-list">
       {tasks.map((task) => (
         <li className="task-item-container" key={task.id}>
-          <input type="checkbox" />
-          <span className="task-item-label"></span>
-          {task.task} - Priority: {task.priority}
+          <div className="task-item">
+            <input type="checkbox" />
+            <span className="task-item-label">{task.title}</span>
+            {task.task} - Priority: {task.priority}
+          </div>
+          <button className="x-button">
+            <svg
+              className="x-button-icon"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
         </li>
       ))}
     </ul>
