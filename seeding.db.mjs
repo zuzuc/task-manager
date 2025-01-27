c45f778c8f0076cbfe797338de07233834a1e167
zuzuc <zuzu@posteo.de>
2025-01-27 16:09:30 +0100

    Add structure to enable dynamic task inclusion

--- seeding.db.mjs
+++ seeding.db.mjs
@@ -1,7 +1,30 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("database.db");
 
+const task = {
+  name: "groceries",
+  priority: "high",
+};
+
 db.serialize(() => {
-  db.run('INSERT INTO tasks VALUES (1, "cleaning", "medium", TRUE, 20250120, 20250120)');
-  db.run('INSERT INTO tasks (id, name, priority) VALUES (2, "dishes", "high")');
+  // Insert hardcoe tasks
+  db.run(
+    'INSERT INTO tasks VALUES (1, "cleaning", "medium", TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)'
+  );
+  db.run(
+    'INSERT INTO tasks (id, name, priority, created_at) VALUES (2, "dishes", "high", CURRENT_TIMESTAMP)'
+  );
+
+  // Insert dynamic task
+  db.run(
+    "INSERT INTO tasks (name, priority) VALUES (?, ?)",
+    [task.name, task.priority],
+    (err) => {
+      if (err) {
+        console.error("Error inserting dynamic task:", err.message);
+      } else {
+        console.log("Dynamic task inserted successfully!");
+      }
+    }
+  );
 });
