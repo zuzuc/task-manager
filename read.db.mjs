485d1e62ea50af3c84e769e4550def6c0c9d48ba
zuzuc <zuzu@posteo.de>
2025-02-03 17:34:29 +0100

    Add SQL to sort for different values

--- read.db.mjs
+++ read.db.mjs
@@ -1,21 +1,63 @@
 import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("database.db");
 
+console.log("=======================================");
 console.log("Take 5 rows with all columns from tasks");
 console.log("=======================================");
 db.serialize(() => {
-  db.each(`SELECT * from tasks`, (err, row) => {
-    console.log(row.id + " status: " + row.status);
+  db.each(`SELECT * from tasks ORDER BY RANDOM() LIMIT 5`, [], (err, row) => {
+    console.log(row.id +  " status: " + row.status + " priority: " + row.priority + " created_at:" + row.created_at);
   });
 });
 
-db.close();
-
-console.log("========================================");
-console.log("Only the rows where the priority is high");
-console.log("========================================");
+console.log("=========================================");
+console.log("Only show rows where the priority is high");
+console.log("=========================================");
 db.serialize(() => {
-  //   db.each(`SELECT * from tasks`, (err, row) => {
-  //     console.log(row.id + " status: " + row.status);
-  //   });
+  db.each(`SELECT * FROM tasks WHERE priority = 'high'`, [], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    console.table(row);
+  });
+});
+
+console.log("==========================================================");
+console.log("Only show rows where task is created after a specific date");
+console.log("==========================================================");
+db.serialize(() => {
+    db.each(`SELECT * FROM tasks WHERE created_at > ? ORDER BY created_at ASC`, ['2025-01-01'], (err, row) => {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        console.table(row);
+    });
+});
+
+console.log("======================================================================");
+console.log("Only show rows where id is higher than 3 and task name is not cleaning");
+console.log("======================================================================");
+db.serialize(() => {
+    db.each(`SELECT * FROM tasks WHERE id > ? AND name != ? ORDER BY id ASC`, [3, 'cleaning'], (err, row) => {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        console.table(row); 
+    });
+});
+
+console.log("=============================================================================");
+console.log("Only show 5 rows: take the 5 latest created tasks and sort by DESC created_at");
+console.log("=============================================================================");
+db.serialize(() => {
+    db.each(`SELECT * FROM tasks ORDER BY created_at DESC LIMIT 5`, [], (err, row) => {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        console.table(row);
+    });
 });
