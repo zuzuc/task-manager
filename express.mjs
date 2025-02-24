2233e0d9eced66e582e57715b0ee11217af804bf
zuzuc <zuzu@posteo.de>
2025-02-24 16:09:29 +0100

    Integrate CORS in Express

--- express.mjs
+++ express.mjs
@@ -1,8 +1,12 @@
 import express from "express";
 import sqlite3 from "sqlite3";
-const db = new sqlite3.Database("database.db");
+import cors from "cors";
 
 const app = express();
+const db = new sqlite3.Database("database.db");
+
+// Use CORS middleware
+app.use(cors());
 
 app.get("/db", (req, res) => {
   db.serialize(() => {
@@ -11,6 +15,16 @@ app.get("/db", (req, res) => {
     });
   });
 });
+
+// app.get("/db", (req, res) => {
+//   db.all(`SELECT * FROM tasks`, (error, rows) => {
+//     if (error) {
+//       res.status(500).json({ error: error.message });
+//     } else {
+//       res.json(rows);
+//     }
+//   });
+// });
 
 app.listen(3001, () => {
   console.log("Task Manager listening");
